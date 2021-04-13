bb0c53754407d7464c84cc762115f4723ae17ea7
Sergei Matveev <sgsq@ya.ru>
2021-04-13 20:35:54 +0400

    Initial

---
+++ Africa/App/AppStart.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Sergei on 27.03.2021.
+//
+
+import SwiftUI
+
+struct AppStart: View {
+    var body: some View {
+      MainView()
+    }
+}
+
+struct AppStart_Previews: PreviewProvider {
+    static var previews: some View {
+       AppStart()
+    }
+}
